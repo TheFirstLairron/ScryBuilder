9224724cca5ed4c550f930259e2aa627cede73bd
Zachary Allnutt <zrwall@hotmail.com>
2019-06-10 22:32:13 -0700

    Setting up initial boilerplate. API routing not properly configured

---
+++ ScryBuilder/ScryBuilder.Server/Controllers/CardsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScryBuilder.Server.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardsController : ControllerBase
+    {
+        [HttpGet]
+        public string Card()
+        {
+            return "SUCCESS";
+        }
+    }
+}
