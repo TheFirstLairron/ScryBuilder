148903d46649a34b39df8e3a096872370de861f9
Zachary Allnutt <zrwall@hotmail.com>
2019-06-30 22:35:46 -0700

    Upgraded to preview 6 and set up some boilerplate

---
+++ ScryBuilder/ScryBuilder.Server/Controllers/CardController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ScryBuilder.Shared.Models;
+using Ignitus.Option;
+
+namespace ScryBuilder.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardController : ControllerBase
+    {
+        public Option<Card> GetCardByName(string name)
+        {
+            return new None<Card>();
+        }
+
+        public List<Option<Card>> GetCardsByName(List<string> names)
+        {
+            return new List<Option<Card>>();
+        }
+    }
+}
---
+++ ScryBuilder/ScryBuilder.Shared/Models/Card.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScryBuilder.Shared.Models
+{
+    public class Card
+    {
+    }
+}
