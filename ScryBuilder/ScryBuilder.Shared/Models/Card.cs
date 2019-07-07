8c9154a3fe8fa69386276208408dc510cec1bbb8
Zachary Allnutt <zrwall@hotmail.com>
2019-07-06 20:57:07 -0700

    Added properties to the card object. Added some routing info onto the card controller.

--- ScryBuilder/ScryBuilder.Shared/Models/Card.cs
+++ ScryBuilder/ScryBuilder.Shared/Models/Card.cs
@@ -6,5 +6,18 @@ namespace ScryBuilder.Shared.Models
 {
     public class Card
     {
+
+        public Guid ID { get; set; }
+
+        public string Name { get; set; }
+
+        public Uri Image { get; set; }
+
+        public Card(Guid id, string name, Uri image)
+        {
+            ID = id;
+            Name = name;
+            Image = image;
+        }
     }
 }
